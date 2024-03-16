96a3802ceb79c004108269b9606b381968095334
Byirosaleshd <68984100+Byirosaleshd@users.noreply.github.com>
2024-03-15 22:37:43 -0400

    Ejecicio simple de sql y importacion de Pandas forzada

---
+++ UDF.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+import pandas as pd
+
+
+
+square = lambda n : n*n
+
+with sqlite3.connect("Northwind.db") as conn:
+    conn.create_function("square",1,square)
+    cursor = conn.cursor()
+    cursor.execute("SELECT *,square(Price) as Precio_al_cuadrado FROM Products WHERE Price != 0")
+    results = cursor.fetchall()
+    results_df = pd.DataFrame(results)
+    print(results_df)
